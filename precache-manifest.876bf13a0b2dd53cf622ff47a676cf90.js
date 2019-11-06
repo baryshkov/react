566610eec7b5225ade41795262a110e97df6b43f
baryshkov <ambaryshkov@gmail.com>
2019-11-06 15:15:33 +0300

    Updates

---
+++ precache-manifest.876bf13a0b2dd53cf622ff47a676cf90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d62d94aa31468a5f506cae9cfb50e70",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "7b29686eed5d599c0a0e",
+    "url": "/to-do/static/css/main.7589bcd1.chunk.css"
+  },
+  {
+    "revision": "d1546cd062b4d81cfba8",
+    "url": "/to-do/static/js/2.5f382e5d.chunk.js"
+  },
+  {
+    "revision": "7b29686eed5d599c0a0e",
+    "url": "/to-do/static/js/main.b0ccf002.chunk.js"
+  },
+  {
+    "revision": "4896a2fd1afe17c65548",
+    "url": "/to-do/static/js/runtime-main.73b29ee3.js"
+  }
+]);
