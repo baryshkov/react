86fdf4b88916481a728920f9ed0cfd83db37a0b1
baryshkov <ambaryshkov@gmail.com>
2019-11-06 15:30:34 +0300

    Updates

--- precache-manifest.3fe1851537d3cfc57f01568f24c43e31.js
+++ precache-manifest.3fe1851537d3cfc57f01568f24c43e31.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9883103a000cf251ddb26ab85e8595f",
+    "revision": "caca471218b388bb62af73caec3456e3",
     "url": "/react-to-do/index.html"
   },
   {
-    "revision": "7b29686eed5d599c0a0e",
+    "revision": "90a239c41f231a60f178",
     "url": "/react-to-do/static/css/main.7589bcd1.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-to-do/static/js/2.5f382e5d.chunk.js"
   },
   {
-    "revision": "7b29686eed5d599c0a0e",
-    "url": "/react-to-do/static/js/main.b0ccf002.chunk.js"
+    "revision": "90a239c41f231a60f178",
+    "url": "/react-to-do/static/js/main.6ee445af.chunk.js"
   },
   {
     "revision": "acf0405d4ed8531f8893",
